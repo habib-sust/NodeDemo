bf4ad4d0f9dcb995700671c1f3b25dc42f91e81b
Habibur Rahamn <shopon2024@gmail.com>
2018-05-27 00:00:51 +0600

    database connected

--- services/BudgetManagerAPI/config/index.js
+++ services/BudgetManagerAPI/config/index.js
@@ -3,5 +3,5 @@
 module.exports = {
 	secret: 'budgetsecret',
 	session: {session: false},
-	database: 'mongodb://127.0.0.1:27017/budgetmanager' 
+	databaseURL: 'mongodb://admin:2024@ds031751.mlab.com:31751/habib' 
 }
